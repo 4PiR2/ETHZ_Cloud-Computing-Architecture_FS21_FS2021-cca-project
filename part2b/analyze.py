4955978b61e9744b04e516e96ef84a5b243fc3fe
CJL-0123456789 <19995777+CJL-0123456789@users.noreply.github.com>
2021-03-22 13:44:35 +0100

    update plot

--- part2b/analyze.py
+++ part2b/analyze.py
@@ -8,7 +8,9 @@ benchmarks = ['blackscholes', 'canneal' ,'dedup', 'ferret', 'fft', 'freqmine']
 n_threads = [1, 2, 4, 8]
 reps = 3
 
-p = re.compile(r'real\s+([0-9]*)m([0-9]*\.[0-9]*)s')
+p = re.compile(r'^real\s+([0-9]+)m([0-9]+\.[0-9]+)s$')
+pfft = re.compile(r'^Total time without initialization :\s+([0-9]+)$')
+pfreqmine = re.compile(r', the FPgrowth cost ([0-9]+\.[0-9]+) seconds$')
 
 result = np.zeros((len(benchmarks), len(n_threads), reps))
 
@@ -17,29 +19,44 @@ for i in range(len(benchmarks)):
 		for rep in range(reps):
 			with open(path_log+benchmarks[i]+'_'+str(n_threads[i_threads])+'_'+str(rep)+'.txt', 'r') as f:
 				for line in f:
-					m=p.match(line)
-					if m:
-						t = int(m.group(1)) * 60 + float(m.group(2))
-						result[i, i_threads, rep] = t
-						break
+					if i == 4:
+						m = pfft.match(line)
+						if m:
+							t = int(m.group(1)) / 1e6
+							result[i,i_threads,rep] = t
+							break
+					elif i == 5:
+						m = pfreqmine.search(line)
+						if m:
+							t = float(m.group(1))
+							result[i,i_threads,rep] = t
+							break
+					else:
+						m=p.match(line)
+						if m:
+							t = int(m.group(1)) * 60 + float(m.group(2))
+							result[i,i_threads,rep] = t
+							break
 
 result_mean = np.mean(result, axis=-1)
 result_base = result_mean[:,[0]*len(n_threads)]
 result_speedup = result_base / result_mean
 result_std = np.std(result, ddof=1 ,axis=-1)
-result_std /= result_base
+result_err = result_std * result_base / result_mean**2
 
-for i in range(len(benchmarks)):
-	plt.plot(n_threads, result_speedup[i], label=benchmarks[i])
+ylim = 6
+plt.plot([0, ylim], [0, ylim], color='#b0b0b0', linewidth=1, linestyle='--')
+for i in np.argsort(result_speedup[:,2])[::-1]:
+	plt.errorbar(n_threads, result_speedup[i], yerr=result_err[i], label=benchmarks[i], marker='.', capsize=2)
 plt.xticks(n_threads)
-plt.yticks(range(6))
+plt.yticks(range(ylim + 1))
 plt.xlim(0, 9)
-plt.ylim(0, 5)
+plt.ylim(0, ylim)
 plt.gca().set_aspect("equal")
 plt.xlabel('# Threads')
 plt.ylabel('Speedup')
 plt.title('PARSEC Parallel Behavior')
-plt.grid()
+plt.grid(linestyle='--')
 plt.legend()
-plt.savefig('plot.png', dpi=300)
+plt.savefig('plot.pdf', bbox_inches='tight', pad_inches=0.1)
 plt.show()
